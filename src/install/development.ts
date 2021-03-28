8eee6c66435fcc23aae9a5ac6271779039543056
Daniel <30466471+daniel0611@users.noreply.github.com>
2021-03-28 15:17:18 +0200

    Add progress bar for cloning

--- src/install/development.ts
+++ src/install/development.ts
@@ -1,3 +1,4 @@
+import { SingleBar } from "cli-progress";
 import * as git from "nodegit";
 import { directoryExists, executeCommand } from "../fsUtils";
 import { DevelopmentInstallation } from "../installation";
@@ -5,27 +6,53 @@ import { DevelopmentInstallation } from "../installation";
 const nodecgIOCloneURL = "https://github.com/codeoverflow-org/nodecg-io.git";
 
 export async function createDevInstall(_info: DevelopmentInstallation, nodecgIODir: string): Promise<void> {
-    await cloneGitRepo(nodecgIODir);
+    await getGitRepo(nodecgIODir);
     await installNPMDependencies(nodecgIODir);
     await buildTypeScript(nodecgIODir);
 }
 
-async function cloneGitRepo(nodecgIODir: string) {
+async function getGitRepo(nodecgIODir: string): Promise<void> {
     if (await directoryExists(nodecgIODir)) {
-        console.log("nodecg-io git repository is already cloned.");
-        console.log("Pulling latest changes...");
-
-        const repo = await git.Repository.open(nodecgIODir);
-        await repo.fetchAll();
-        const branch = await repo.head();
-        await repo.mergeBranches(branch, `origin/${branch.name()}`);
-
-        console.log("Succesfully pulled latest changes form GitHub.");
-        return;
+        await pullRepo(nodecgIODir);
+    } else {
+        await cloneRepo(nodecgIODir);
     }
+}
 
+async function pullRepo(nodecgIODir: string): Promise<void> {
+    console.log("nodecg-io git repository is already cloned.");
+    console.log("Pulling latest changes...");
+
+    const repo = await git.Repository.open(nodecgIODir);
+    await repo.fetchAll();
+    const branch = await repo.head();
+    await repo.mergeBranches(branch, `origin/${branch.name()}`);
+
+    console.log("Succesfully pulled latest changes from GitHub.");
+}
+
+async function cloneRepo(nodecgIODir: string): Promise<void> {
     console.log("Cloning nodecg-io git repository...");
-    await git.Clone.clone(nodecgIOCloneURL, nodecgIODir); // TODO: this sometimes completely locks up, maybe also add a progress indicator
+
+    const bar = new SingleBar({
+        format: "Cloning... {value}/{total} objects [{bar}] {percentage}%",
+    });
+    bar.start(0, 0);
+
+    await git.Clone.clone(nodecgIOCloneURL, nodecgIODir, {
+        fetchOpts: {
+            callbacks: {
+                // Wohoooo broken typings....
+                // eslint-disable-next-line @typescript-eslint/no-explicit-any
+                transferProgress: (a: any) => {
+                    bar.setTotal(a.totalObjects());
+                    bar.update(a.receivedObjects());
+                },
+            },
+        },
+    });
+
+    bar.stop();
     console.log("Cloned nodecg-io git repository.");
 }
 
