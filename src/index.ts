6e1270c3a9d7e8e0329585405a4ef16f8a747473
Daniel <30466471+daniel0611@users.noreply.github.com>
2021-04-28 12:37:44 +0200

    Add minimum node version check

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,11 @@ import * as yargs from "yargs";
 import { installModule } from "./install";
 import { uninstallModule } from "./uninstall";
 import { version } from "../package.json";
-import { checkForCliUpdate } from "./cliUpdateCheck";
+import { checkForCliUpdate, ensureNode12 } from "./cli";
 
 // This file gets imported by the index.js file of the repository root.
 
+ensureNode12();
 checkForCliUpdate();
 
 yargs(process.argv.slice(2))
