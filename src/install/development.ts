codeoverflow-org/nodecg-io-cli

commit a593c421d128eb469d8ecea53e37be47fcf86c46

Run npm install and build after generating a bundle

src/install/development.ts
@@ -7,6 +7,7 @@ import { DevelopmentInstallation, writeInstallInfo } from "../utils/installation
 import { logger } from "../utils/log";
 import * as path from "path";
 import * as glob from "glob";
+import { runNpmBuild, runNpmInstall } from "../utils/npm";
 
 type CloneRepository = "nodecg-io" | "nodecg-io-docs";
 const nodecgIOCloneURL = "https://github.com/codeoverflow-org/nodecg-io.git";
@@ -176,7 +177,7 @@ function getGitCommitHash(dir: string): Promise<string> {
 async function installNPMDependencies(nodecgIODir: string) {
     logger.info("Installing npm dependencies and bootstrapping...");
 
-    await executeCommand("npm", ["install"], nodecgIODir);
+    await runNpmInstall(nodecgIODir, false);
     await executeCommand("npm", ["run", "bootstrap"], nodecgIODir);
 
     logger.info("Installed npm dependencies and bootstrapped.");
@@ -184,6 +185,6 @@ async function installNPMDependencies(nodecgIODir: string) {
 
 async function buildTypeScript(nodecgIODir: string, concurrency: number) {
     logger.info("Compiling nodecg-io...");
-    await executeCommand("npm", ["run", "build", "--", "--concurrency", concurrency.toString()], nodecgIODir);
+    await runNpmBuild(nodecgIODir, "--", "concurrency", concurrency.toString());
     logger.success("Compiled nodecg-io.");
 }

src/generate/index.ts
@@ -3,12 +3,13 @@ import * as chalk from "chalk";
 import * as path from "path";
 import * as fs from "fs";
 import { logger } from "../utils/log";
-import { directoryExists, findNodeCGDirectory, getNodeCGIODirectory } from "../utils/fs";
+import { directoryExists, executeCommand, findNodeCGDirectory, getNodeCGIODirectory } from "../utils/fs";
 import { ProductionInstallation, readInstallInfo } from "../utils/installation";
 import { corePackages, getServiceClientName } from "../nodecgIOVersions";
 import { GenerationOptions, promptGenerationOpts } from "./prompt";
 import * as defaultTsConfigJson from "./tsconfig.json";
 import CodeBlockWriter from "code-block-writer";
+import { runNpmBuild, runNpmInstall } from "../utils/npm";
 
 export const yellowInstallCommand = chalk.yellow("nodecg-io install");
 const yellowGenerateCommand = chalk.yellow("nodecg-io generate");
@@ -73,8 +74,13 @@ async function generateBundle(
     await generatePackageJson(bundlePath, opts);
     await generateTsConfig(bundlePath);
     await generateExtension(bundlePath, opts, install);
+    logger.info("Generated bundle successfully.");
 
-    // TODO: perform npm install and first build
+    logger.info("Installing dependencies...");
+    await runNpmInstall(bundlePath, false);
+
+    logger.info("Compiling bundle...");
+    await runNpmBuild(bundlePath);
 }
 
 async function generatePackageJson(bundlePath: string, opts: GenerationOptions): Promise<void> {
@@ -152,7 +158,7 @@ async function generateExtension(
             writer.blankLine();
 
             writer
-                .write(`${svc.camelCase}?.onAvailable(async (${svc.camelCase}Client) =>`)
+                .write(`${svc.camelCase}?.onAvailable(async (${svc.camelCase}Client) => `)
                 .inlineBlock(() => {
                     writer.writeLine(`nodecg.log.info("${svc.name} client has been updated.")`);
                     writer.writeLine(`// You can now use the ${svc.name} client here.`);

src/install/production.ts
@@ -149,7 +149,7 @@ async function createSymlinks(pkgs: NpmPackage[], nodecgIODir: string): Promise<
  */
 async function installNpmDependencies(pkgs: NpmPackage[], nodecgIODir: string): Promise<void> {
     await writeWorkspacePackageJson(pkgs, nodecgIODir);
-    await runNpmInstall(nodecgIODir);
+    await runNpmInstall(nodecgIODir, true);
 }
 
 /**

src/utils/npm.ts
@@ -150,9 +150,20 @@ export async function downloadNpmPackage(pkg: NpmPackage, nodecgIODir: string): 
 /**
  * Installs npm production dependencies in the passed path by running npm install --prod in the directory.
  * @param path the path where a package.json is present
+ * @param prod whether to only install production dependencies or also devDependencies.
  */
-export async function runNpmInstall(path: string): Promise<void> {
-    await executeCommand("npm", ["install", "--prod"], path);
+export async function runNpmInstall(path: string, prod: boolean): Promise<void> {
+    const prodArg = prod ? ["--prod"] : [];
+    await executeCommand("npm", ["install", ...prodArg], path);
+}
+
+/**
+ * Compiles the TypeScript code of a package by running executing the npm script called "build".
+ * @param path the path in which the package is located.
+ * @param args additional arguments that are passed to npm.
+ */
+export async function runNpmBuild(path: string, ...args: string[]): Promise<void> {
+    await executeCommand("npm", ["run", "build", ...args], path);
 }
 
 /**

test/install/production.ts
@@ -98,10 +98,10 @@ describe("installPackages", () => {
         expect(packageJson["workspaces"]).toStrictEqual(packages.map((p) => p.path));
     });
 
-    test("should install dependencies", async () => {
+    test("should install prod dependencies", async () => {
         await installPackages(packages, createInstall(), nodecgIODir);
         expect(npmInstallMock).toHaveBeenCalled();
-        expect(npmInstallMock).toHaveBeenCalledWith(nodecgIODir);
+        expect(npmInstallMock).toHaveBeenCalledWith(nodecgIODir, true);
     });
 
     test("should revert changes if npm install fails", async () => {

test/utils/npm/pkgManagement.ts
@@ -1,5 +1,11 @@
 import { createFsFromVolume, vol } from "memfs";
-import { createNpmSymlinks, getSubPackages, removeNpmPackage, runNpmInstall } from "../../../src/utils/npm";
+import {
+    createNpmSymlinks,
+    getSubPackages,
+    removeNpmPackage,
+    runNpmBuild,
+    runNpmInstall,
+} from "../../../src/utils/npm";
 import { tempDir, corePkg, fsRoot, twitchChatPkg, dashboardPkg } from "../../testUtils";
 import * as fsUtils from "../../../src/utils/fs";
 import * as path from "path";
@@ -7,13 +13,37 @@ import * as path from "path";
 jest.mock("fs", () => createFsFromVolume(vol));
 afterEach(() => vol.reset());
 
+const npmCommand = "npm";
+const execMock = jest.spyOn(fsUtils, "executeCommand").mockResolvedValue();
+
+afterEach(() => execMock.mockClear());
+
 describe("runNpmInstall", () => {
-    test("should execute npm install", async () => {
-        const spy = jest.spyOn(fsUtils, "executeCommand").mockResolvedValue();
-        await runNpmInstall(fsRoot);
-        expect(spy).toHaveBeenCalled();
-        expect(spy.mock.calls[0][0]).toBe("npm");
-        expect(spy.mock.calls[0][1][0]).toBe("install");
+    test("should execute npm install --prod when installing prod only", async () => {
+        await runNpmInstall(fsRoot, true);
+        expect(execMock).toHaveBeenCalled();
+        expect(execMock.mock.calls[0][0]).toBe(npmCommand);
+        expect(execMock.mock.calls[0][1][0]).toBe("install");
+        expect(execMock.mock.calls[0][1][1]).toBe("--prod");
+        expect(execMock.mock.calls[0][2]).toBe(fsRoot);
+    });
+
+    test("should execute npm install when installing prod and dev", async () => {
+        await runNpmInstall(fsRoot, false);
+        expect(execMock).toHaveBeenCalled();
+        expect(execMock.mock.calls[0][0]).toBe(npmCommand);
+        expect(execMock.mock.calls[0][1][0]).toBe("install");
+        expect(execMock.mock.calls[0][1].length).toBe(1);
+    });
+});
+
+describe("runNpmBuild", () => {
+    test("should execute install script with passed arguments", async () => {
+        await runNpmBuild(fsRoot, "arg");
+        expect(execMock).toHaveBeenCalled();
+        expect(execMock.mock.calls[0][0]).toBe(npmCommand);
+        expect(execMock.mock.calls[0][1][1]).toBe("build");
+        expect(execMock.mock.calls[0][1][2]).toBe("arg"); // Custom arg from above
     });
 });
 

commit 508d8e3cda6b9eabd1c99d33aa3fb170a18ecc1b

fix(install): fix incorrect concurrency option when creating a dev install
Creating a development install would result in an error because the dashes of the concurrency option that is passed through npm to lerna was missing the two dashes.
This bug was introduced in a refactoring in a593c421d128eb469d8ecea53e37be47fcf86c46.

src/install/development.ts
@@ -185,6 +185,6 @@ async function installNPMDependencies(nodecgIODir: string) {
 
 async function buildTypeScript(nodecgIODir: string, concurrency: number) {
     logger.info("Compiling nodecg-io...");
-    await runNpmBuild(nodecgIODir, "--", "concurrency", concurrency.toString());
+    await runNpmBuild(nodecgIODir, "--", "--concurrency", concurrency.toString());
     logger.success("Compiled nodecg-io.");
 }