ea957f9986aed26b31afd361bb22e37ac7c49266
Daniel Huber <30466471+daniel0611@users.noreply.github.com>
2022-01-27 17:19:16 +0100

    Add integration test for install command

---
+++ integration_test.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# This is a very basic integration test that ensures that the nodecg-io cli can be used
+# to install nodecg-io to a NodeCG installation and (if a production installation is used)
+# succesfully generate a new bundle.
+# This is cun in a matrix in CI to ensure that it works for windows and ubuntu
+# and with all supported nodecg-io versions.
+set -e
+
+version="$1"
+if [ -z "$version" ]; then
+  echo "Usage: integration_test.sh <nodecg-io-version>"
+  exit 1
+fi
+
+dir=$(mktemp -d)
+
+echo "Test directory: $dir"
+function clean_test_directory {
+    echo "Cleaning test directory at $dir"
+    rm -rf $dir
+}
+trap clean_test_directory EXIT
+
+cd $dir
+git clone https://github.com/nodecg/nodecg.git --depth 1 .
+
+if [ "$version" == "development" ]; then
+    nodecg-io install --nodecg-io-version $version --docs
+else
+    nodecg-io install --nodecg-io-version $version --all-services
+fi
+
+nodecg-io uninstall
+
